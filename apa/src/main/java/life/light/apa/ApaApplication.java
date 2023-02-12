cddd32119d5b13346fc2d348455d96a1fd7a6fd9
life light <lifelightdev@gmail.com>
2023-02-12 16:47:08 +0100

    Initial commit

---
+++ apa/src/main/java/life/light/apa/ApaApplication.java
@@ -0,0 +1,13 @@
+package life.light.apa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApaApplication.class, args);
+	}
+
+}
---
+++ apa/src/test/java/life/light/apa/ApaApplicationTests.java
@@ -0,0 +1,13 @@
+package life.light.apa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
